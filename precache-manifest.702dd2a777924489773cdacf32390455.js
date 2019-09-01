2486d510ffd99ebbf697c3bcfc0235259ee00ced
mukasa7din <mukasa7din@github.io>
2019-08-31 23:36:43 -0700

    Updates

---
+++ precache-manifest.702dd2a777924489773cdacf32390455.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "61ce9c1c0d1bb08740f95a5019b5941e",
+    "url": "/static/media/sunny.61ce9c1c.jpeg"
+  },
+  {
+    "revision": "589d623ed002a700e7ce102cb071ac61",
+    "url": "/static/media/rainy.589d623e.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e3412bb220499a4b55d",
+    "url": "/static/js/main.df3fe38b.chunk.js"
+  },
+  {
+    "revision": "1e28a31ca8a25c329997",
+    "url": "/static/js/2.744f4309.chunk.js"
+  },
+  {
+    "revision": "6e3412bb220499a4b55d",
+    "url": "/static/css/main.5d019c74.chunk.css"
+  },
+  {
+    "revision": "f8c9588210eb636daf4ad8f941a17cd3",
+    "url": "/index.html"
+  }
+];
